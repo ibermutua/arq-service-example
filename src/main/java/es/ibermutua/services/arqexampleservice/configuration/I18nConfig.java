50f83500f09267218beb527dc2fb79307c17fba7
Jorge Fernandez <jorgefernandez@ibermutua.es>
2019-09-30 12:16:01 +0200

    internacionalizacion.

---
+++ src/main/java/es/ibermutua/services/arqexampleservice/configuration/I18nConfig.java
@@ -0,0 +1,17 @@
+package es.ibermutua.services.arqexampleservice.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class I18nConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+        return messageSource;
+    }
+}
